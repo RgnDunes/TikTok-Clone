4ce8390b3f26dcf2fef459ed0fdee2fa6c484e20
RgnDunes <singh.divyansh1802@gmail.com>
2021-05-04 02:46:48 +0530

    Added snap style in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
 import React from "react";
+import Video from "./Video";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "./firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJFCl3SmfeHSmMCSvI88alTXCsV5FcUlU",
+  authDomain: "dunes-tiktok-clone-react.firebaseapp.com",
+  projectId: "dunes-tiktok-clone-react",
+  storageBucket: "dunes-tiktok-clone-react.appspot.com",
+  messagingSenderId: "437239702203",
+  appId: "1:437239702203:web:ad3975a254f59111b5bf9e",
+  measurementId: "G-SDVXVG928E",
+};
