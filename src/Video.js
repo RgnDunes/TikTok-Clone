7c736549717a5d75c096e7e57a39a2fd7edc92ad
RgnDunes <singh.divyansh1802@gmail.com>
2021-05-04 03:08:41 +0530

    Added Video Component

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
 
 function Video() {
+  const videoRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.playing();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="video">
-      <h1>Video</h1>
+      <video
+        ref={videoRef}
+        onClick={onVideoPress}
+        loop
+        className="video__player"
+        src="https://www.youtube.com/embed/GePLvNgWROg"
+      />
     </div>
   );
 }
